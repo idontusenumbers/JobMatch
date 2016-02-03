2c615992cb84432f985780728990b0e4f097b985
Charlie Hayes <cosmotic@gmail.com>
2016-02-02 22:47:07 -0600

    reenabled min/max and seed script

--- jobmatch/src/main/resources/data.sql
+++ jobmatch/src/main/resources/data.sql
@@ -1,20 +1,18 @@
 --
 -- Seed the Role database with possible user roles
 --
--- MERGE INTO ROLE AS t USING (VALUES(1,TRIM('admin')), (2, TRIM('student')), (3, TRIM('employer'))) AS vals(id,name)
--- ON t.ID=vals.id
--- WHEN MATCHED THEN UPDATE SET t.NAME=vals.name
--- WHEN NOT MATCHED THEN INSERT VALUES vals.id, vals.name;
+MERGE INTO ROLE AS t USING (VALUES(1,TRIM('admin')), (2, TRIM('student')), (3, TRIM('employer'))) AS vals(id,name)
+ON t.ID=vals.id
+WHEN MATCHED THEN UPDATE SET t.NAME=vals.name
+WHEN NOT MATCHED THEN INSERT VALUES vals.id, vals.name;
 
 --
 -- Seed the skill table with possible user skills
 --
--- MERGE INTO SKILL AS t USING (VALUES(1,TRIM('C++')), (2, TRIM('Java'))) AS vals(a,b)
--- ON t.ID=vals.a
--- WHEN MATCHED THEN UPDATE SET t.NAME=vals.b
--- WHEN NOT MATCHED THEN INSERT VALUES vals.a, vals.b;
+MERGE INTO SKILL AS t USING (VALUES(1,TRIM('C++')), (2, TRIM('Java'))) AS vals(a,b)
+ON t.ID=vals.a
+WHEN MATCHED THEN UPDATE SET t.NAME=vals.b
+WHEN NOT MATCHED THEN INSERT VALUES vals.a, vals.b;
 
 
--- placeholders so the script doesnt error
-CREATE TABLE dummy (id INT PRIMARY KEY, description VARCHAR(100));
-DROP TABLE dummy;
+
