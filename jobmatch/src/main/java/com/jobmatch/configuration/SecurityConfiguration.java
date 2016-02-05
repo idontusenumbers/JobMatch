3b5d2da99a836d37e5b10c537fac53616565b773
Charlie Hayes <cosmotic@gmail.com>
2016-02-04 20:21:57 -0600

    Added js and styles to permitall

--- jobmatch/src/main/java/com/jobmatch/configuration/SecurityConfiguration.java
+++ jobmatch/src/main/java/com/jobmatch/configuration/SecurityConfiguration.java
@@ -18,7 +18,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 				.authorizeRequests()
-				.antMatchers("/", "/home").permitAll()
+				.antMatchers("/", "/home", "/styles/**", "/js/**").permitAll()
 				.anyRequest().authenticated()
 				.and()
 				.formLogin()
