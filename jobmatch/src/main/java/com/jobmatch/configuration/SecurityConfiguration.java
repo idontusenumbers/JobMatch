10289163cdf01bcc151e1d431783082d018d3232
Eric Pfeiffer <computerfreak@computerfr33k.com>
2016-02-04 20:32:26 -0600

    remove inmemory authentication since we are storing users in database.

--- jobmatch/src/main/java/com/jobmatch/configuration/SecurityConfiguration.java
+++ jobmatch/src/main/java/com/jobmatch/configuration/SecurityConfiguration.java
@@ -6,8 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -33,15 +33,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
 	}
-
-
-	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth
-				.inMemoryAuthentication()
-				.withUser("user").password("password").roles("USER");
-	}
-
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
