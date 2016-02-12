629f46f709af669ada98f9d5a3cb7375cbe49e62
Eric Pfeiffer <computerfreak@computerfr33k.com>
2016-02-11 20:46:00 -0600

    removed extra semi-colon from file. Removed /home from being allowed without auth. Logout now works from above fix.

--- jobmatch/src/main/java/com/jobmatch/configuration/SecurityConfiguration.java
+++ jobmatch/src/main/java/com/jobmatch/configuration/SecurityConfiguration.java
@@ -26,11 +26,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 				.authorizeRequests()
-				.antMatchers("/", "/home", "/register", "/styles/**", "/js/**").permitAll()
+				.antMatchers("/", "/register", "/styles/**", "/js/**").permitAll()
 				.anyRequest().authenticated()
 				.and().formLogin().loginPage("/login").permitAll()
 				.and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login");
-		;
 	}
 
 	@Autowired
