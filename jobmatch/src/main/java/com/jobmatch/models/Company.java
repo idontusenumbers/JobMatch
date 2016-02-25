a547c8fffbaafdda4b0f554e9b18c3e32efc9f3c
Charlie Hayes <cosmotic@gmail.com>
2016-02-24 18:37:08 -0600

    Added company model and repository

---
+++ jobmatch/src/main/java/com/jobmatch/models/Company.java
@@ -0,0 +1,79 @@
+package com.jobmatch.models;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private String phone;
+    private String address;
+    private String zipcode;
+    private String website;
+
+    public Company(String name, String phone, String address, String zipcode, String website) {
+        this.name = name;
+        this.phone = phone;
+        this.address = address;
+        this.zipcode = zipcode;
+        this.website = website;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Company{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", phone='" + phone + '\'' +
+                ", address='" + address + '\'' +
+                ", zipcode='" + zipcode + '\'' +
+                ", website='" + website + '\'' +
+                '}';
+    }
+}
---
+++ jobmatch/src/main/java/com/jobmatch/repositories/CompanyRepository.java
@@ -0,0 +1,10 @@
+package com.jobmatch.repositories;
+
+import com.jobmatch.models.Company;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompanyRepository extends CrudRepository<Company, Integer> {
+
+}
