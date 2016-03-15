0e0d2555faf9beefc05172882a7222a8daf05be5
Emilia Paulski <epaulski@mail.depaul.edu>
2016-03-15 09:56:30 -0500

    minor color changes; added more culture options

--- jobmatch/src/main/resources/data.sql
+++ jobmatch/src/main/resources/data.sql
@@ -19,7 +19,7 @@ WHEN NOT MATCHED THEN INSERT VALUES vals.a, vals.b;
 --
 -- Seed the culture table with possible user skills
 --
-MERGE INTO CULTURE AS t USING (VALUES(1,'Work from home'), (2, 'Casual dress')) AS vals(a,b)
+MERGE INTO CULTURE AS t USING (VALUES(1,'Work from home'), (2, 'Casual dress'),(3, 'Creative') ,(4, 'Logical') ,(5, 'Problem Solver') ,(6, 'Good Listener') ,(7, 'Oral Communicator') ,(8, 'Written Communicator') ,(9, 'Flexibility in Scheduling') ,(10, 'People Skills') ,(11, 'Punctual') ,(12, 'Accurate') ,(13, 'Efficient') ,(14, 'Multi-Tasker') ,(15, 'Analytical') ,(16, 'Planner') ,(17, 'Researcher') ,(18, 'Team Leader') ,(19, 'Team Member') ,(20, 'Stamina') ,(21, 'Like Fast Pace') ,(22, 'Realationship Builder') ,(23, 'UX/UI') ,(24, 'Teacher/Mentor')) AS vals(a,b)
 ON t.ID=vals.a
 WHEN MATCHED THEN UPDATE SET t.NAME=vals.b
 WHEN NOT MATCHED THEN INSERT VALUES vals.a, vals.b;
