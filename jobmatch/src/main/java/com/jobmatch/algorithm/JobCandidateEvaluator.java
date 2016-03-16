8611548642069c30663f12079d56f490120b20e8
Emilia Paulski <epaulski@mail.depaul.edu>
2016-03-16 17:52:19 -0500

    fixed algo?

--- jobmatch/src/main/java/com/jobmatch/algorithm/JobCandidateEvaluator.java
+++ jobmatch/src/main/java/com/jobmatch/algorithm/JobCandidateEvaluator.java
@@ -4,6 +4,7 @@ import com.jobmatch.models.JobPost;
 import com.jobmatch.models.RankedSkill;
 import com.jobmatch.models.RankedCulture;
 import com.jobmatch.models.User;
+import org.springframework.util.CollectionUtils;
 
 import java.util.*;
 
@@ -43,33 +44,58 @@ public class JobCandidateEvaluator {
     }
 
     static public int findDistance(User user, JobPost jobPost) {
-        int result = 0;
         Set<RankedSkill> us = user.getSkills();
         Set<RankedSkill> jps = jobPost.getSkills();
         Set<RankedCulture> ugc = user.getCultures();
         Set<RankedCulture> jpgc = jobPost.getCreator().getCompany().getCultures();
 
-        // TODO using hashmaps might make this more efficient
+        HashSet<RankedSkill> hashsetUserSkill = new HashSet<>();
+        HashSet<RankedSkill> hashsetJobSkill = new HashSet<>();
+        HashSet<RankedCulture> hashsetUserCulture = new HashSet<>();
+        HashSet<RankedCulture> hashsetJobCulture = new HashSet<>();
 
-        for (RankedSkill jp : jps) {
-            for (RankedSkill u : us) {
-                if (jp.equals(u)) {
-                    result += Math.abs(jp.getRank() - u.getRank());
-                }else{
-                    result+=MIN_DISTANCE;
-                }
-            }
+        for (RankedSkill u : us) {
+            hashsetUserSkill.add(u);
         }
+        for (RankedSkill j : jps) {
+            hashsetJobSkill.add(j);
+        }
+        int sizeBeforeJobSkill = hashsetJobSkill.size();
+        hashsetJobSkill.retainAll(hashsetUserSkill);
+        int sizeAfterJobSkill = hashsetJobSkill.size();
 
+
+        for (RankedCulture u : ugc) {
+            hashsetUserCulture.add(u);
+        }
         for (RankedCulture j : jpgc) {
-            for (RankedCulture u : ugc) {
-                if (j.equals(u)) {
-                    result += Math.abs(j.getRank() - u.getRank());
-                }else{
-                    result+=MIN_DISTANCE;
-                }
-            }
+            hashsetJobCulture.add(j);
         }
+        int sizeBeforeJobCulture = hashsetJobCulture.size();
+        hashsetJobCulture.retainAll(hashsetUserCulture);
+        int sizeAfterJobCulture = hashsetJobCulture.size();
+
+        int result = ((sizeBeforeJobSkill - sizeAfterJobSkill) + (sizeBeforeJobCulture - sizeAfterJobCulture)) * MIN_DISTANCE;
+
+//        for (RankedSkill jp : jps) {
+//            for (RankedSkill u : us) {
+//                if (jp.equals(u)) {
+//                    result += Math.abs(jp.getRank() - u.getRank());
+//                }else{
+//                    result+=MIN_DISTANCE;
+//                }
+//            }
+//        }
+//
+//        for (RankedCulture j : jpgc) {
+//            for (RankedCulture u : ugc) {
+//                if (j.equals(u)) {
+//                    result += Math.abs(j.getRank() - u.getRank());
+//                }else{
+//                    result+=MIN_DISTANCE;
+//                }
+//            }
+//        }
         return result;
     }
 }
